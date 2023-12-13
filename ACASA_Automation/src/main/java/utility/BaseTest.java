1a9ec7f5d3e99deee06becee311fc32da48ca977
Usman <md.usman2195@gmail.com>
2023-12-13 16:44:13 +0530

    add base tst class

--- ACASA_Automation/src/main/java/utility/BaseTest.java
+++ ACASA_Automation/src/main/java/utility/BaseTest.java
@@ -1,5 +1,27 @@
 package utility;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 
+public class BaseTest 
+{
+	public static WebDriver driver = null;
+	public static WebDriverWait wait;
+	
+	@BeforeClass
+	public void openBrowser() 
+	{	
+		driver = new ChromeDriver();
+		driver.get("https://www.acasa.ae/");
+	}
+	
+	@AfterClass
+	public void closeBrowser() 
+	{
+		driver.quit();
+	}
+	
 }
